dc18e7b70a976089a1ff3a1f4f40176a94ba66cf
marcos <marcosguzzz@gmail.com>
2021-11-06 10:19:04 -0500

    twittor

--- main.go
+++ main.go
@@ -1,5 +1,16 @@
 package main
 
+import (
+	"log"
+
+	"github.com/marcosguz/twittor/bd"
+	"github.com/marcosguz/twittor/handlers"
+)
+
 func main(){
-	
+	if bd.ChequeoConnection() == 0 {
+		log.Fatal("Sin conexión a la BD")
+		return
+	}
+	handlers.Manejadores()
 }
